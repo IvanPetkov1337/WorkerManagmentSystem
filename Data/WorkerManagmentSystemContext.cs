efae68a6bf8e8eb5e30f85771abf8f533af1627d
Ivan Petkov <55034308+IvanPetkov1337@users.noreply.github.com>
2022-11-03 01:54:28 +0200

    Add project files.

---
+++ Data/WorkerManagmentSystemContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WorkerManagmentSystem.Data
+{
+    public class WorkerManagmentSystemContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public WorkerManagmentSystemContext() : base("name=WorkerManagmentSystemContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<WorkerManagmentSystem.Models.Worker> Workers { get; set; }
+    }
+}
---
+++ Models/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WorkerManagmentSystem.Models
+{
+    public class Worker
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        public double Salary { get; set; }
+        public string Position { get; set; }
+    }
+}
